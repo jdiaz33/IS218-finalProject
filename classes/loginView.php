f5bc00960c634bb9f240b62566c5697488d91bed
jdiaz33 <josed0331@hotmail.com>
2016-04-22 22:23:32 -0400

    added the login form

--- classes/loginView.php
+++ classes/loginView.php
@@ -1,37 +1,47 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
-
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="../css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
-        
-        <!-- Add your site or application content here -->
-        
 
 	<?php
 	  class loginView {
 	    
-	    public function getHeader() {
-	      $title = '<h1> IS 218 from login</h1>';
-	      return $title;
+	    public function getLoginPage() {
+	      
+	      $page = '<!DOCTYPE html>
+	        <html lang="en">
+	        <head>
+		  <meta charset="utf-8">
+		  <title>IS 218 Login</title>
+		  <meta http-equiv="x-ua-compatible" content="ie=edge">
+		  <meta name="viewport" content="width=device-width,
+		  initial-scale=1">
+		  <link rel="stylesheet" href="css/main.css">
+		  <link rel="stylesheet" href="css/login.css">
+		</head>
+		<body>
+		  <div class="container">
+		    <section id="content">
+		      <form action="">
+		        <h1>Login Form</h1>
+			<div>
+			  <input type="text" placeholder="Username" required=""
+			  id="username" />
+			</div>
+			<div>
+			  <input type="password" placeholder="Password" required=""
+			  id="password" />
+			</div>
+			<div>
+			  <input type="submit" value="Log in" />
+			  <a href="#">Lost your password?</a>
+			  <a href="#">Register</a>
+			</div>
+		      </form>
+		      <div class="button">
+		        <a href="#">Download sourcefile</a>
+		      </div>
+		    </section>
+		  </div>
+		</body>
+	      </html>';
+
+	      return $page;
 	    }
 	  }
-	?>
-        
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-    </body>
-</html>
-<?php
