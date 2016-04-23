6b2ab0e1ff44d059bdbbafcc66969bdbd7581247
jdiaz33 <josed0331@hotmail.com>
2016-04-23 01:06:35 -0400

    add configuration to DB

---
+++ classes/config.php
@@ -0,0 +1,8 @@
+<?php
+  
+  define('DB_SERVER', 'sql1.njit.edu');
+  define('DB_USERNAME', 'jld33');
+  define('DB_PASSWORD', 'CsjGVvOb');
+  define('DB_DATABASE', 'jld33');
+  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
