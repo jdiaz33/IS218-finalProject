efb1a3ccb7c54b813d91cbf5be5aaea8074b130a
jdiaz33 <josed0331@hotmail.com>
2016-04-25 12:48:31 -0400

    if statement to login or create a new user

--- classes/loginCtrl.php
+++ classes/loginCtrl.php
@@ -18,35 +18,36 @@
 	      define('DB_PASSWORD', 'CsjGVvOb');
 	      define('DB_DATABASE', 'jld33');
 	      $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-	     
+
+	      if($_POST['form'] == login) {
 	      
-	      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-	      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+	        $myusername = mysqli_real_escape_string($db,$_POST['username']);
+	        $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 		
-
-	      $sql = "SELECT Username FROM User WHERE Username = '$myusername' and
-	      Password = '$mypassword'";
-	      $result = mysqli_query($db, $sql);
-	      print_r($result);
-	      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	        $sql = "SELECT Username FROM User WHERE Username = '$myusername' and
+	        Password = '$mypassword'";
+	        $result = mysqli_query($db, $sql);
+	        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 	      
-	      $active = $row['active'];
-
-	      $count = mysqli_num_rows($result);
+	        #$active = $row['active'];
+	        $count = mysqli_num_rows($result);
 	      
 	// If result matched $myusername and $mypassword, table row must be 1 row
-	      if($count == 1) {
-	        session_start();
+	        if($count == 1) {
+	          session_start();
 		
-		#session_register("myusername");
-		print_r($_SESSION);
-		$_SESSION['login_user'] = $myusername;
-		print_r($_SESSION);
-		#header("location: index.php?controller=homepageCtrl");
+		  #session_register("myusername");
+		  $_SESSION['login_user'] = $myusername;
+		  print_r($_SESSION);
+		  #header("location: index.php?controller=homepageCtrl");
+	        }
+	        else {
+		  $error = "your Login Name or Password is invalid";
+	          echo $error;
+	        }
 	      }
 	      else {
-		$error = "your Login Name or Password is invalid";
-	        echo $error;
+		echo 'registering';
 	      }
 	    }
 
