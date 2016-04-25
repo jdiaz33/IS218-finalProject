9ef414aca2439d630e146653bea68cbda9aad412
jdiaz33 <josed0331@hotmail.com>
2016-04-25 12:25:34 -0400

    database setup and first call

--- classes/loginCtrl.php
+++ classes/loginCtrl.php
@@ -1,25 +1,6 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
-
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="../css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
-        
-        <!-- Add your site or application content here -->
-        
 
 	<?php
+
 	  class loginCtrl extends controller {
 	    
 	    public function get() {
@@ -30,15 +11,45 @@
 	      $this->html .= $loginPage;
 	    }
 
-	    public function post() {}
+	    public function post() {
+	      
+	      define('DB_SERVER', 'sql1.njit.edu');
+	      define('DB_USERNAME', 'jld33');
+	      define('DB_PASSWORD', 'CsjGVvOb');
+	      define('DB_DATABASE', 'jld33');
+	      $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	     
+	      
+	      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+	      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+		
+
+	      $sql = "SELECT Username FROM User WHERE Username = '$myusername' and
+	      Password = '$mypassword'";
+	      $result = mysqli_query($db, $sql);
+	      print_r($result);
+	      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	      
+	      $active = $row['active'];
+
+	      $count = mysqli_num_rows($result);
+	      
+	// If result matched $myusername and $mypassword, table row must be 1 row
+	      if($count == 1) {
+	        session_start();
+		
+		#session_register("myusername");
+		print_r($_SESSION);
+		$_SESSION['login_user'] = $myusername;
+		print_r($_SESSION);
+		#header("location: index.php?controller=homepageCtrl");
+	      }
+	      else {
+		$error = "your Login Name or Password is invalid";
+	        echo $error;
+	      }
+	    }
+
 	    public function put() {}
 	    public function delete() {}
 	  }
-	?>
-        
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-    </body>
-</html>
-<?php
