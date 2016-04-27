65e962dc5bea73640146b1a6905c69b6b12194ef
jdiaz33 <josed0331@hotmail.com>
2016-04-27 00:05:55 -0400

    complete registration form

--- classes/loginCtrl.php
+++ classes/loginCtrl.php
@@ -24,22 +24,20 @@
 	        $myusername = mysqli_real_escape_string($db,$_POST['username']);
 	        $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 		
-	        $sql = "SELECT Username FROM User WHERE Username = '$myusername' and
-	        Password = '$mypassword'";
+	        $sql = "SELECT userName, userId FROM carUser WHERE userName = '$myusername' and password = '$mypassword'";
 	        $result = mysqli_query($db, $sql);
 	        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 	      
-	        #$active = $row['active'];
 	        $count = mysqli_num_rows($result);
 	      
 	// If result matched $myusername and $mypassword, table row must be 1 row
 	        if($count == 1) {
 	          session_start();
 		
-		  #session_register("myusername");
 		  $_SESSION['login_user'] = $myusername;
-		  print_r($_SESSION);
-		  #header("location: index.php?controller=homepageCtrl");
+		  $_SESSION['user_id'] = $row['userId'];
+		  
+		  header("location: index.php?controller=homepageCtrl");
 	        }
 	        else {
 		  $error = "your Login Name or Password is invalid";
@@ -47,22 +45,33 @@
 	        }
 	      }
 	      else {
-		$myusername = mysqli_real_escape_string($db,$_POST['username']);
-		$mypassword = mysqli_real_escape_string($db,$_POST['password']);
-		$mypassword2 = mysqli_real_escape_string($db,$_POST['password2']);
+		    $name = mysqli_real_escape_string($db,$_POST['name']);
+		    $lastName = mysqli_real_escape_string($db,$_POST['lastName']); 
+		    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+		    $email = mysqli_real_escape_string($db,$_POST['email']);
+		    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+		    $mypassword2 = mysqli_real_escape_string($db,$_POST['password2']);
 
 		if($mypassword == $mypassword2) {
-		  $sql = "INSERT INTO User (Username, Password) VALUES
-		  ('$myusername', '$mypassword')";
+		  $id = uniqid();
+		  $sql = "INSERT INTO carUser (userId, name, lastName, userName, email, password) VALUES('$id', '$name', '$lastName', '$myusername', '$email', '$mypassword')";
 		  if($db->query($sql) === TRUE) {
 		    echo "New record created successfully";
+		    session_start();
+		    $_SESSION['login_user'] = $myusername;
+		    $_SESSION['user_id'] = $id;
+		    header("location: index.php?controller=homepageCtrl");
 		  }
 		  else {
 		    echo "Error: " . $sql . "<br>" . $db->error;
 		  }
 		}
-	      }
-	    }
+		else {
+		  $error = "Passwords don't match up";
+		  echo $error;
+		}
+      }
+    }
 
 	    public function put() {}
 	    public function delete() {}
