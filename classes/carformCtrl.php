8e3a734d53995c2850dc80106674c6b659570794
jdiaz33 <josed0331@hotmail.com>
2016-04-27 17:58:03 -0400

    fix get method on carform

--- classes/carformCtrl.php
+++ classes/carformCtrl.php
@@ -1,20 +1,25 @@
 <?php
-  class carformCtrl extends controller {
-    public function get() {
-     session_start();
-     
-     $carForm = new carformView;
-     
-     $pageHeader = $carForm->getHeader();
-     $this->html .= $pageHeader;
-     
-     $navBar = $carForm->getNavBar();
-	 $this->html .= $navBar;
-	 
-	 $footer = $carForm->getFooter();
-	 $this->html .= $footer;
-    }
-    public function post() {}
-    public function put() {}
-    public function delete() {}
+ 
+ class carformCtrl extends controller {
+  
+  public function get() {
+   
+   session_start();
+   
+   $carform = new carformView;
+
+   $pageHeader = $carform->getHeader();
+   $this->html .= $pageHeader;
+
+   $navBar = $carform->getNavBar();
+   $this->html .= $navBar;
+
+   $footer = $carform->getFooter();
+   $this->html .= $footer;
   }
+
+  public function post() {}
+  public function put() {}
+  public function delete() {}
+
+ }
