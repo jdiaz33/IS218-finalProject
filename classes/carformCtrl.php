e1cb15423625e95a8402a515bc650f240444533c
jdiaz33 <josed0331@hotmail.com>
2016-04-27 23:30:43 -0400

    setup data to make the sql query

--- classes/carformCtrl.php
+++ classes/carformCtrl.php
@@ -24,21 +24,25 @@
   public function post() {
    session_start();
    
+   //VIN from the post form
+   $vin = $_POST['vin'];
+   
+   //set cURL to call Edmunds API
    $ch = curl_init();
-
    curl_setopt($ch, CURLOPT_URL,
-   "https://api.edmunds.com/api/vehicle/v2/vins/1FAHP26W49G252740?fmt=json&api_key=u48d7aetdk4yz6pwqnzd62ez");
+   "https://api.edmunds.com/api/vehicle/v2/vins/$vin?fmt=json&api_key=u48d7aetdk4yz6pwqnzd62ez");
    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
    $output = curl_exec($ch);
-
    curl_close($ch);
    
+   //response from cURL calling the Edmunds API
    $response = json_decode($output);
-   print_r($response);
-   echo '<br>'.$response->make->name;
-   echo '<br>'.$response->model->name;
-   echo '<br>'.$response->years[0]->year;
-    
+   
+   //setup data to make the DB query
+   $userid = $_SESSION['user_id'];
+   $make = $response->make->name;
+   $modle = $response->model->name;
+   $year = $response->years[0]->year;
   }
 
   public function put() {}
