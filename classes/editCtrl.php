3190bcc409ebefa04f88312bd4fb474aed3137be
jdiaz33 <josed0331@hotmail.com>
2016-05-10 12:10:11 -0400

    edit file

---
+++ classes/editCtrl.php
@@ -0,0 +1,27 @@
+<?php
+  
+  class editCtrl extends controller {
+  
+    public function get() {
+    
+      session_start();
+      
+      $editPage = new editView;
+      
+      $pageHeader = $editPage->getHeader();
+   	  $this->html .= $pageHeader;
+   	  
+   	  $navBar = $editPage->getNavBar();
+      $this->html .= $navBar;
+      
+      $footer = $editPage->getFooter();
+      $this->html .= $footer;
+    
+    }
+    
+    public function post() {}
+    
+    public function put() {}
+    public function delete() {}
+    
+  }
