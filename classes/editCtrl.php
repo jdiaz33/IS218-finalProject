5fdf4590632d32b7ff44feeeef78a503c706bf60
jdiaz33 <josed0331@hotmail.com>
2016-05-10 12:32:31 -0400

    cURL call to Edmunds

--- classes/editCtrl.php
+++ classes/editCtrl.php
@@ -14,7 +14,19 @@
    	  $navBar = $editPage->getNavBar();
       $this->html .= $navBar;
       
-      $body = $editPage->getBody();
+      $vin = $_GET['vin'];
+      //set cURL to call Edmunds API
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,
+      "https://api.edmunds.com/api/vehicle/v2/vins/$vin?fmt=json&api_key=u48d7aetdk4yz6pwqnzd62ez");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $output = curl_exec($ch);
+      curl_close($ch);
+      
+      //response from cURL calling the Edmunds API
+      $response = json_decode($output);
+      
+      $body = $editPage->getBody($response);
       $this->html .= $body;
       
       $footer = $editPage->getFooter();
