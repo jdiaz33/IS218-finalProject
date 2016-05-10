9bf3562b108be497839bb2deb30a173e8435ba93
jdiaz33 <josed0331@hotmail.com>
2016-05-10 16:15:00 -0400

    edit car condition View

--- classes/editView.php
+++ classes/editView.php
@@ -33,19 +33,33 @@
   }
   
   
-  public function getBody($car) {
+  public function getBody($car, $vin) {
+
+    $carMake = $car['Make'];
+    $carModel = $car['Model'];
+    $carPrice = $car['Price'];
+    
     $body = '
      <br><br><br>
      <div style="width:60%; margin:auto; background-color:#003366;
      border-radius:5px; border: 2px solid black; padding:15px; padding-bottom:40px">
-      <h2 style="color:white">'.$car->make->name.'&nbsp'.$car->model->name.'</h2>
+      <h2 style="color:white">'.$carMake.'&nbsp'.$carModel.'</h2>
       <br>
       <div style="width:70%; margin:auto; text-align:center">
        <form action="index.php?controller=editCtrl" method="post">
-        <input type="text" placeholder="New Price" name="vin" style="width:50%;
+        <h3 style="color:white">New Price: </h3><br>
+        <input type="text" placeholder="New Price" name="price" value="'.$carPrice.'"style="width:50%;
 	height:30px;
         background-color:gray; border:2px solid black; font-size:20px; color:white;
         padding:5px; border-radius:5px">
+        <br><br><br>
+        <select name="cond" style="width:50%; height:30px; background-color:gray;
+	border:2px
+	solid black; color:white; padding:5px; font-size:15px; font-weight:bold">
+	 <option  value="used">Used</option>
+	 <option  value="new">New</option>
+	</select>
+        <input type="hidden" name="vin" value="'.$vin.'">
 	<br><br><br>
 	<button type="submit" style="background-color:gray; color:white;
 	font-weight:bold; font-size:19px; width:120px; height:35px; border:2px solid
@@ -53,6 +67,7 @@
        </form>
        <br>
        <form action="index.php?controller=editCtrl&action=delete" method="post">
+         <input type="hidden" name="vin" value="'.$vin.'">
          <button type="submit" style="background-color:#cc0000; color:white; font-weight:bold; font-size:19px; width:220px; height:35px; border:2px solid black; border-radius:5px">Delete Car</button>
        </form>
       </div>
